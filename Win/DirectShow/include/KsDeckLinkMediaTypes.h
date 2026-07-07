50caacff446fec6401a01b08d33603947aa41165
Your Name <you@example.com>
2026-07-07 16:46:11 +0300

    migrate to 16.0

--- Win/DirectShow/include/KsDeckLinkMediaTypes.h
+++ Win/DirectShow/include/KsDeckLinkMediaTypes.h
@@ -1,5 +1,5 @@
 /* -LICENSE-START-
-** Copyright (c) 2026 Blackmagic Design
+** Copyright (c) 2014 Blackmagic Design
 **
 ** Permission is hereby granted, free of charge, to any person or organization
 ** obtaining a copy of the software and accompanying documentation covered by
@@ -25,59 +25,16 @@
 ** -LICENSE-END-
 */
 
-/*
- * -- AUTOMATICALLY GENERATED - DO NOT EDIT ---
- */
+#include <guiddef.h>
 
-#ifndef BMD_DECKLINKAPIDISCOVERY_H
-#define BMD_DECKLINKAPIDISCOVERY_H
+// {43594448-0000-0010-8000-00AA00389B71}
+DEFINE_GUID(BMD_MEDIASUBTYPE_HDYC, 
+	0x43594448, 0x0000, 0x0010, 0x80, 0x00, 0x00, 0xAA, 0x00, 0x38, 0x9B, 0x71);
 
+// {30313276-0000-0010-8000-00AA00389B71}
+DEFINE_GUID(BMD_MEDIASUBTYPE_v210a, 
+	0x30313276, 0x0000, 0x0010, 0x80, 0x00, 0x00, 0xAA, 0x00, 0x38, 0x9B, 0x71);
 
-#ifndef BMD_CONST
-    #if defined(_MSC_VER)
-        #define BMD_CONST __declspec(selectany) static const
-    #else
-        #define BMD_CONST static const
-    #endif
-#endif
-
-#ifndef BMD_PUBLIC
-	#define BMD_PUBLIC
-#endif
-
-// Type Declarations
-
-
-// Interface ID Declarations
-
-BMD_CONST REFIID IID_IDeckLink                                    = /* C418FBDD-0587-48ED-8FE5-640F0A14AF91 */ { 0xC4,0x18,0xFB,0xDD,0x05,0x87,0x48,0xED,0x8F,0xE5,0x64,0x0F,0x0A,0x14,0xAF,0x91 };
-
-#if defined(__cplusplus)
-
-// Forward Declarations
-
-class IDeckLink;
-
-/* Interface IDeckLink - Represents a DeckLink device */
-
-class BMD_PUBLIC IDeckLink : public IUnknown
-{
-public:
-    virtual HRESULT GetModelName (/* out */ CFStringRef* modelName) = 0;
-    virtual HRESULT GetDisplayName (/* out */ CFStringRef* displayName) = 0;
-
-protected:
-    virtual ~IDeckLink () {} // call Release method to drop reference count
-};
-
-/* Functions */
-
-extern "C" {
-
-
-}
-
-
-
-#endif /* defined(__cplusplus) */
-#endif /* defined(BMD_DECKLINKAPIDISCOVERY_H) */
+// {30313272-0000-0010-8000-00AA00389B71}
+DEFINE_GUID(BMD_MEDIASUBTYPE_r210, 
+	0x30313272, 0x0000, 0x0010, 0x80, 0x00, 0x00, 0xAA, 0x00, 0x38, 0x9B, 0x71);
